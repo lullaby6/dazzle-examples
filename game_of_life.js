9289b57a5327da0d8b9dab018932aa855c2402e1
LucianoBrumer <lucianobrumer5@gmail.com>
2023-10-08 15:23:38 -0300

    refactoring gof

--- game_of_life.js
+++ game_of_life.js
@@ -53,11 +53,11 @@ const Cell = {
             if(event.button === 0) {
                 current.alive = true
                 current.scene.board[current.boardPos.x][current.boardPos.y] = 1
-                current.color = current.alive ? '#000' : '#fff'
+                current.color = '#000'
             }else {
                 current.alive = false
                 current.scene.board[current.boardPos.x][current.boardPos.y] = 0
-                current.color = current.alive ? '#000' : '#fff'
+                current.color = '#fff'
             }
         }
     }
@@ -71,45 +71,8 @@ const game = new Game({
         main: {
             custom: {
                 board: [],
-                boardPause: false
-            },
-            load: current => {
-                //creating BOARD_SIZE * BOARD_SIZE board
-                for (let row = 0; row < BOARD_SIZE; row++) {
-                    const row = new Array(BOARD_SIZE)
-                    for (let col = 0; col < row.length; col++) {
-                        row[col] = 0;
-                    }
-                    current.board.push(row)
-                }
-
-                //creating cells
-                for (let row_index = 0; row_index < current.board.length; row_index++){
-                    for (let col_index = 0; col_index < current.board[row_index].length; col_index++){
-                        const createdCell = current.instantGameObject({
-                            ...Cell,
-                            x: row_index * CELL_SIZE,
-                            y: col_index * CELL_SIZE,
-                        })
-
-                        //50% chance of alive
-                        const aliveChance = randomItemFromArray([true, false])
-                        if(aliveChance) {
-                            createdCell.alive = true
-                            //updating board
-                            current.board[row_index][col_index] = 1
-                        }
-                    }
-                }
-            },
-            update: current => {
-                const cells = current.getGameObjectByTag('cell')
-
-                cells.forEach(cell => cell.checkNieghbors(cell))
-                if(!current.boardPause) cells.forEach(cell => cell.updateLife(cell))
-            },
-            keyDown: ({event, current}) => {
-                if(event.key == 'r') {
+                boardPause: false,
+                resetCells: current => {
                     const cells = current.getGameObjectByTag('cell')
 
                     cells.forEach(cell => {
@@ -125,6 +88,40 @@ const game = new Game({
 
                     cells.forEach(cell => cell.checkNieghbors(cell))
                     cells.forEach(cell => cell.updateLife(cell))
+                }
+            },
+            load: current => {
+                //creating board
+                for (let row = 0; row < BOARD_SIZE; row++) {
+                    const row = new Array(BOARD_SIZE)
+                    for (let col = 0; col < row.length; col++) {
+                        row[col] = 0;
+                    }
+                    current.board.push(row)
+                }
+
+                //creating cells
+                for (let row_index = 0; row_index < current.board.length; row_index++){
+                    for (let col_index = 0; col_index < current.board[row_index].length; col_index++){
+                        current.instantGameObject({
+                            ...Cell,
+                            x: row_index * CELL_SIZE,
+                            y: col_index * CELL_SIZE,
+                        })
+                    }
+                }
+
+                current.resetCells(current)
+            },
+            update: current => {
+                const cells = current.getGameObjectByTag('cell')
+
+                cells.forEach(cell => cell.checkNieghbors(cell))
+                if(!current.boardPause) cells.forEach(cell => cell.updateLife(cell))
+            },
+            keyDown: ({event, current}) => {
+                if(event.key == 'r') {
+                    current.resetCells(current)
                 }else if(event.key == 'p') current.boardPause = !current.boardPause
             }
         }
